be38d4289756263f01de0622f13bdd4748762499
david uche <smurojan@gmail.com>
2024-10-01 16:54:33 +0200

    seed and home page

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,15 +1,9 @@
 class GamesController < ApplicationController
-
   def index
+    @games = []
+    @game_players = GamePlayer.where(user_id: current_user.id)
+    @game_players.each do |game_player|
+      @games << game_player.game
+    end
   end
-
-  def show
-  end
-
-  def new
-  end
-
-  def create
-  end
-
 end
